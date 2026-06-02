5c2b15c15b37ed693499338e0ed59c941452c8eb
Pham Quoc Huy <itachihuy212@gmail.com>
2026-06-02 10:07:51 +0700

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- core/coreapp/views.py
+++ core/coreapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'coreapp/home.html')
+
+def scan_demo(request):
+    return render(request, 'coreapp/scan.html')
+
+def login_view(request):
+    return render(request, 'coreapp/login.html')
+
+def register_view(request):
+    return render(request, 'coreapp/register.html')
+
+def dashboard_view(request):
+    return render(request, 'coreapp/dashboard.html')
+
+def coa_view(request):
+    return render(request, 'coreapp/form1.html')
